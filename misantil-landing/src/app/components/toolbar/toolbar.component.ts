7fb05a927b5ef0cd14f4d93864b847e0e634bbc6
Franco Osores <franco_osores@hotmail.com>
2024-12-02 19:58:16 -0300

    first commit

---
+++ misantil-landing/src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+
+}

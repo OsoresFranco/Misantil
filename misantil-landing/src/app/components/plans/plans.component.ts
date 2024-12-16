f0b0873d0df2891e71be5c3825c31f2c4a376582
Roberto-Osores <osoresroberto@gmail.com>
2024-12-16 19:09:22 -0300

    Finished the plans component

--- misantil-landing/src/app/components/plans/plans.component.ts
+++ misantil-landing/src/app/components/plans/plans.component.ts
@@ -12,17 +12,17 @@ export class PlansComponent {
   items = [
     {
       title: 'Entrenamiento personalizado presencial',
-      description: `<p>Objetivos Orientados: salud, estética corporal, rendimiento deportivo.</p> <p>Optimización de las Variables de programación del Entrenamiento.</p> <p>Horario y Lugar a coordinar (San Miguel deTucumán - Yerba Buena).</p> <p>Atención por cupos reducidos.<br> Control y valoración mensual de progresos en base a los objetivos.</p> <p>Estrategias nutricionales según objetivos planteados (opcional).</p> <p>Asesoramiento en suplementación (opcional).</p>`,
+      description: `<div>Objetivos Orientados: salud, estética corporal, rendimiento deportivo.</div> <div>Optimización de las Variables de programación del Entrenamiento.</div> <div>Horario y Lugar a coordinar (San Miguel deTucumán - Yerba Buena).</div> <div>Atención por cupos reducidos.<br> Control y valoración mensual de progresos en base a los objetivos.</div> <div>Estrategias nutricionales según objetivos planteados (opcional).</div> <div>Asesoramiento en suplementación (opcional).</div>`,
     },
     {
       title: 'Entrenamiento personalizado online',
       description:
-        '<p>Objetivos: salud, estética corporal, rendimiento deportivo.</p><p>Optimizacion de las Variables del Entrenamiento periódicamente.</p><p>⁠Encuentro por video llamada una vez por mes, para revisión y análisis del proceso.</p><p>Resolución de consultas, por WhatsApp.</p><p>Control y valoración mensual de progresos en base a los objetivos.</p><p>Soporte en video de los ejercicios seleccionados para tu planificación.</p><p>Estrategias nutricionales según objetivos planteados (opcional).</p><p>Asesoramiento en  suplementación (opcional).</p>',
+        '<div>Objetivos: salud, estética corporal, rendimiento deportivo.</div><div>Optimizacion de las Variables del Entrenamiento periódicamente.</div><div>⁠Encuentro por video llamada una vez por mes, para revisión y análisis del proceso.</div><div>Resolución de consultas, por WhatsApp.</div><div>Control y valoración mensual de progresos en base a los objetivos.</div><div>Soporte en video de los ejercicios seleccionados para tu planificación.</div><div>Estrategias nutricionales según objetivos planteados (opcional).</div><div>Asesoramiento en  suplementación (opcional).</div>',
     },
     {
       title: 'Modalidad híbrida (online + presencial)',
       description:
-        '<p>El servicio combina la flexibilidad del seguimiento online y la guía de sesiones presencial.</p> <p>Supervisión periódica en gimnasios y/o lugares acondicionados para entrenamientos con sobrecarga (San Miguel de Tucumán - Yerba Buena).</p><p>Optimización de las Variables del Entrenamiento.</p><p>Control y valoración mensual de progresos en base a los objetivos.</p><p>⁠Soporte en video de los ejercicios seleccionados en tu programa.</p><p>Estrategias nutricionales según objetivos planteados (opcional).</p><p>⁠Asesoramiento en  suplementación (opcional).</p>',
+        '<div>El servicio combina la flexibilidad del seguimiento online y la guía de sesiones presencial.</div> <div>Supervisión periódica en gimnasios y/o lugares acondicionados para entrenamientos con sobrecarga (San Miguel de Tucumán - Yerba Buena).</div><div>Optimización de las Variables del Entrenamiento.</div><div>Control y valoración mensual de progresos en base a los objetivos.</div><div>⁠Soporte en video de los ejercicios seleccionados en tu programa.</div><div>Estrategias nutricionales según objetivos planteados (opcional).</div><div>⁠Asesoramiento en  suplementación (opcional).</div>',
     },
   ];
 }

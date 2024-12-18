a8c2d331b3c98a2d5e42e6241a76572b65c22179
Roberto-Osores <osoresroberto@gmail.com>
2024-12-18 03:34:46 -0300

    Minor fixes to contact component functionality

--- misantil-landing/src/app/components/contact/contact.component.ts
+++ misantil-landing/src/app/components/contact/contact.component.ts
@@ -17,19 +17,15 @@ export class ContactComponent {
   };
 
   onSubmit() {
-    // Construct the WhatsApp message
     const whatsappMessage = `Hola, mi nombre es ${this.contactData.name}
     Telefono: ${this.contactData.phone}
     Mail: ${this.contactData.email}
     Mensaje: ${this.contactData.message}`;
 
-    // Encode the message to be URL-safe
     const encodedMessage = encodeURIComponent(whatsappMessage);
 
-    // WhatsApp URL with the encoded message
     const whatsappUrl = `https://wa.me/5493816506456?text=${encodedMessage}`;
 
-    // Redirect to WhatsApp
     window.open(whatsappUrl, '_blank');
   }
 }

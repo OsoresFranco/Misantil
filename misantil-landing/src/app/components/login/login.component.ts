5468e3a2dac23e971a6747f9f15dc6f357fc3d2a
Roberto-Osores <osoresroberto@gmail.com>
2024-12-20 16:12:45 -0300

    Added reactive forms to the login component

--- misantil-landing/src/app/components/login/login.component.ts
+++ misantil-landing/src/app/components/login/login.component.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  loginData = {
-    username: '',
-    password: '',
-  };
+  loginForm: FormGroup;
 
-  onSubmit() {}
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted:', this.loginForm.value);
+      
+    } else {
+      this.markAllInputsAsTouched();
+      console.error('Invalid form submission');
+    }
+  }
+  private markAllInputsAsTouched(): void {
+    Object.keys(this.loginForm.controls).forEach((controlName) => {
+      this.loginForm.get(controlName)?.markAsTouched();
+    });
+  }
 }
